e1ce28d664ee609335440f1f0eb5405f359afeaa
Shihmalcom <malcom.shihembetsa@gmail.com>
2022-12-13 03:34:07 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'print alphabets in lower case but q and e'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = o; i < 26; i++)
+	if (i != 4 && i != 16)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
