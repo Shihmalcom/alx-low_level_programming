a9ca2cfef7b9dc1ddaa70ccd4986224b55470edf
Shihmalcom <malcom.shihembetsa@gmail.com>
2022-12-13 10:47:25 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'prints lowercase except for e and q
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp = 'z'; alp++)
+	{
+		if (alp != 'e' && alp != 'z')
+			putchar(alp);
+	}
+	putchar('\n');
+	return (0);
+}
