965da4fa05a10351e9a9114cabe734cf9be8ac4f
Shihmalcom <malcom.shihembetsa@gmail.com>
2022-12-14 09:21:38 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,11 +6,11 @@
  *
  * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
 	int c;
 
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
 		return (1);
 	else
 		return (0);
