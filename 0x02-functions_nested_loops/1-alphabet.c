74a213278eb0a48f81a5a2844a291dc855d1a9f4
Shihmalcom <malcom.shihembetsa@gmail.com>
2022-12-14 04:01:06 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
- * main - entry point
+ * print_alphabet - entry point
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void);
+
+void print_alphabet(void)
+
 {
 	char a = 'a';
 
