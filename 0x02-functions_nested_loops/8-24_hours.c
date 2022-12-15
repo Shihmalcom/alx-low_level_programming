e9be515338537c64bc4063a552fc829fd8ee3349
Shihmalcom <malcom.shihembetsa@gmail.com>
2022-12-14 23:42:54 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,8 +3,8 @@
 /**
  * jack_bauer - function that prints every minute of the day of Jack Bauer,
  * starting from 00:00 to 23:59.
- * m = minute, h = hour
- * /10 allows second digit to rotate
+ * h = hour, m = minute
+ * / 10 allows second digit to rotate
  * for loop breaks before passing 24:00
  *
  * Return: 24 hour clock line by line
