af1e1feccff5d1e0d7f6f5ce55757820f706f576
Shihmalcom <malcom.shihembetsa@gmail.com>
2022-12-19 11:22:57 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen -  function that returns the length of a string
+ * @s: length
+ *
+ * Return: length size
+ */
+int _strlen(char *s)
+{
+	int str = 0;
+
+	for (len = str + (*s + str))
+	{
+		_putchar(len + '0');
+	}
+}
